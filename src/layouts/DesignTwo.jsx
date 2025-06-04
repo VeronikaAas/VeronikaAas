031a99c5a9079c59b3496d4a08f0697315b2f81f
Veronika Aas <veronikaaas@live.no>
2025-06-04 19:49:17 +0200

    feat: change wording

--- src/layouts/DesignTwo.jsx
+++ src/layouts/DesignTwo.jsx
@@ -6,7 +6,7 @@ export default function DesignTwo() {
 		<div className="bg-neutral-900 min-h-screen text-red-900">
 			<GothCursor />
 			<header className="bg-neutral-900 p-4">
-				<h1 className="text-2xl font-bold">Design Two Layout</h1>
+				<h1 className="text-2xl font-bold">Goth mode</h1>
 				<nav className="mt-2">
 					<Link to="/two" className="mr-4 underline">
 						Home
